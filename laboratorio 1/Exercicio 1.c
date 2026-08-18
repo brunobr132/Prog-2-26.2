e050ca7408bf92915725e1ba6ccdd80cdfca0cc6
Bruno Vargas <br10ferreira@gmail.com>
2026-08-18 09:57:21 -0300

    Add files via upload

---
+++ laboratorio 1/Exercicio 1.c
@@ -0,0 +1,67 @@
+// Escreva um programa em C que recebe do usuário (entrada padrão) duas
+// strings e verifica se a segunda é uma substring da primeira. As strings podem
+// ser qualquer palavra ou frase. Para isso, faça o seguinte:
+// 1. Crie uma função ocorrencia que recebe como argumentos duas strings e
+// retorna um inteiro. Caso a segunda string seja uma substring da primeira, a
+// função deverá retornar 1; zero caso contrário. A função deve garantir que a
+// busca não seja sensível a letras maiúsculas e minúsculas (case insensitive). 
+// 2. A função main do programa deve chamar a função ocorrencia para a
+// verificação e imprimir o resultado na saída padrão. Exemplos:
+// ENTRADA SAÍDA
+// "Programacao de Computadores", "prog"        1
+// "Programacao de Computadores", "Comp"        1
+// "Padaria da esquina",          "padariaa"    0
+// "Padaria da esquina",          "esq"         1
+// "Teste de rererememorar",      "rerememorar" 1
+
+#include <stdio.h>
+#include <ctype.h>
+
+int ocorrencia(char string1[51], char string2[51]){
+    char temp1 [51],temp2[51];
+    int i, j, k;
+
+    for(i = 0; string1[i] != '\0'; i++){
+        temp1[i] = tolower(string1[i]);
+
+    };
+
+    //printf("%s", temp1); verificar se a string1 esta sendo passado para o diminutivo
+
+
+    for(i = 0; string2[i] != '\0'; i++){
+        temp2[i] = tolower(string2[i]);
+
+    };
+
+    //printf("%s", temp2); verificar se a string2 esta sendo passado para o diminutivo
+    int contador;
+    for(i=0; temp1 != '\0'; i++){
+        contador =0;
+        for (j = 0; temp2[j] != '\0'; j++){
+            if(temp1[j+i]==temp2[j]){
+                contador++;    
+                break;}
+        }
+        if(contador = strlen(temp2)){
+            return 1;
+        }
+    }return 0;
+    
+
+    
+
+};
+
+
+int main(){
+    char string1[51], string2[51];
+    puts("Escreve uma frase:");
+    fgets(string1, 51, stdin);
+
+    puts("Escreve uma palavra para ser encontrada na frase:");
+    fgets(string2, 51, stdin);
+
+    int resultado =  ocorrencia(string1,string2);
+    printf("resultado: %d \n", resultado);
+}; 
